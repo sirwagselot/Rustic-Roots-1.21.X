daebe2228cab397b9073554bc8f44b36958b750f
Ryan <272399536+sirwagselot@users.noreply.github.com>
2026-03-30 20:19:12 -0700

    Initial commit

---
+++ src/main/java/net/sirwagselot/rusticroots/RusticRoots.java
@@ -0,0 +1,16 @@
+package net.sirwagselot.rusticroots;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RusticRoots implements ModInitializer {
+	public static final String MOD_ID = "rusticroots";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sirwagselot/rusticroots/RusticRootsClient.java
@@ -0,0 +1,10 @@
+package net.sirwagselot.rusticroots;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RusticRootsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
